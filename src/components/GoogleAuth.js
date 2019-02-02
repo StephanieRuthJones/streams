58b7f98dfd2917a814525b2e973c23dfdcfaa7b6
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-02-02 15:19:28 -0700

    Added google sign-in button with conditional for signed in/out messages

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,16 +1,48 @@
 import React from 'react'
 
 class GoogleAuth extends React.Component {
+    state = { isSignedIn: null }
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
-            window.gapi.client.init({
-                clientId: '288682915180-3mnalgj92hjd5ksia58coi1ni5523utc.apps.googleusercontent.com',
-                scope: 'email'
-            })
+            window.gapi.client
+                .init({
+                    clientId: '288682915180-3mnalgj92hjd5ksia58coi1ni5523utc.apps.googleusercontent.com',
+                    scope: 'email'
+                }).then(() => {
+                    this.auth = window.gapi.auth2.getAuthInstance()
+                    this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                    this.auth.isSignedIn.listen(this.onAuthChange)
+                })
         })
     }
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    }
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return null
+        } else if (this.state.isSignedIn) {
+            return (
+                <button className="ui red google button">
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            )
+        } else {
+            return (
+                <button className="ui red google button">
+                    <i className="google icon" />
+                    Sign In
+                </button>
+            )
+        }
+    }
+
     render() {
-        return <div>GoogleAuth</div>
+        return <div>{this.renderAuthButton()}</div>
     }
 }
 export default GoogleAuth
